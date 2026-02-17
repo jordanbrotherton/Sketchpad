cfe3be6c968e36c67ef1f6598769395f57b5ab60
Jordan Brotherton <135299181+jordanbrotherton@users.noreply.github.com>
2026-02-17 18:23:52 -0500

    fix: Extras menu rapid close

--- views/editor/containers/edit_extras/edit_extras.gd
+++ views/editor/containers/edit_extras/edit_extras.gd
@@ -14,20 +14,22 @@ func attach_project(project: Project):
 
 ## Opens the extras panel.
 func open():
-	visible = true
+	if !visible:
+		visible = true
 
-	var top = get_viewport_rect().size.y
-	position.y = top
+		var top = get_viewport_rect().size.y
+		position.y = top
 
-	var tween = create_tween().set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT)
-	tween.tween_property(self, "position:y", 0, anim_speed)
+		var tween = create_tween().set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT)
+		tween.tween_property(self, "position:y", 0, anim_speed)
 
 ## Closes and hides the extras panel.
 func close():
-	var top = get_viewport_rect().size.y
+	if visible:
+		var top = get_viewport_rect().size.y
 
-	var tween = create_tween().set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT)
-	tween.tween_property(self, "position:y", top, anim_speed)
-	await tween.finished
+		var tween = create_tween().set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT)
+		tween.tween_property(self, "position:y", top, anim_speed)
+		await tween.finished
 
-	visible = false
+		visible = false
