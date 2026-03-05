79dccdaebec692fc24386f0b76fca42e5a1e1862
Jordan Brotherton <135299181+jordanbrotherton@users.noreply.github.com>
2026-03-04 23:22:12 -0500

    feat(page): Added flatten function

--- system/page/page.gd
+++ system/page/page.gd
@@ -4,10 +4,11 @@ extends Resource
 @export var layers: Array[Image] = []
 var textures: Array[ImageTexture] = []
 
-func _init(w: int, h: int) -> void:
-	create_layer(w, h, Color.WHITE)
-	create_layer(w, h)
-	print("[Page] Page created")
+func _init(w: int = 0, h: int = 0) -> void:
+	if w != 0 and h != 0:
+		create_layer(w, h, Color.WHITE)
+		create_layer(w, h)
+		print("[Page] Page created")
 
 ## Creates a layer in the page. [br]
 ## [param w] - Width of layer. [br]
@@ -23,6 +24,8 @@ func get_content() -> Array[ImageTexture]:
 	for i in range(layers.size()):
 		if i < textures.size():
 			textures[i].update(layers[i])
+		else:
+			textures.append(ImageTexture.create_from_image(layers[i]))
 	return textures
 
 ## Updates Page contents with live textures.
@@ -37,3 +40,16 @@ func set_content() -> void:
 func set_layer(idx: int, image: Image) -> void:
 	layers[idx] = image
 	textures[idx].update(layers[idx])
+	
+## Flattens the page into a single page. [br]
+## Useful for exports and thumbnails.
+func flatten() -> Image:
+	var out = layers[0].duplicate()
+	for i in range(1, len(layers)):
+		var img = layers[i]
+		out.blend_rect(
+			img,
+			Rect2(Vector2.ZERO, img.get_size()),
+			Vector2.ZERO
+		)
+	return out
